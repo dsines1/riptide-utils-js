d95a8de668fe75ade9b77dcd5cb4f99e8a7971c7
Jesse Slavens <jesse.slavens@riptidesoftware.com>
2016-06-14 16:16:06 -0400

    Updated array.js based on Tyler's suggestion

--- utils/array.js
+++ utils/array.js
@@ -8,187 +8,140 @@
 (function(proto) {
   "use strict";
 
-  Object.defineProperty(proto, 'first', {
-    value: function(predicate) {
-      for (var i = 0; i < this.length; ++i) {
-        if (predicate(this[i])) {
-          return this[i];
-        }
-      }
-      return undefined;
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
+  function defineProp(name, val){
+    return Object.defineProperty(proto, name, {
+       value: val,
+       writable: false,
+       enumerable: false,
+       configurable: false
+     })
+  };
 
-  Object.defineProperty(proto, 'remove', {
-    value: function(item) {
-      var index = this.indexOf(item);
-      if (index === -1) {
-        return;
+  var firstFunc = function(predicate) {
+    for (var i = 0; i < this.length; ++i) {
+      if (predicate(this[i])) {
+        return this[i];
       }
-      this.splice(index, 1);
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
+    }
+    return undefined;
+  };
+
+  var removeFunc = function(item) {
+    var index = this.indexOf(item);
+    if (index === -1) {
+      return;
+    }
+    this.splice(index, 1);
+  };
 
   //pushes a value only if it is not in the array
-  Object.defineProperty(proto, 'pushUnique', {
-    value: function(item) {
-      if (!this.inArray(item)) {
-        this.push(item);
-      }
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  })
+  var pushUniqueFunc = function(item) {
+    if (!this.inArray(item)) {
+      this.push(item);
+    }
+  };
 
   // returns a new array of unique values
-  Object.defineProperty(proto, 'unique', {
-    value: function() {
-      var uniqueArray = [];
-      this.forEach(function(item) {
-        uniqueArray.pushUnique(item);
-      });
-      return uniqueArray;
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
+  var uniqueFunc = function() {
+    var uniqueArray = [];
+    this.forEach(function(item) {
+      uniqueArray.pushUnique(item);
+    });
+    return uniqueArray;
+  };
 
-  Object.defineProperty(proto, 'intersect', {
-    value: function(array) {
-      var intersection = [];
-      array.forEach(function(item) {
-        if (this.inArray(item)) {
-          intersection.push(item);
-        }
-      });
-      return intersection;
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
-
-  Object.defineProperty(proto, 'inArray', {
-    value: function(item) {
-      return this.indexOf(item) !== -1;
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
-
-  Object.defineProperty(proto, 'outer', {
-    value: function(array) {
-      var outer = [];
-      this.forEach(function(item) {
-        if (!array.inArray(item)) {
-          outer.push(item);
-        }
-      });
-      return outer;
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
-
-  //shuffles the items in an array;
-  Object.defineProperty(proto, 'shuffle', {
-    value: function() {
-      var i = this.length,
-        j, temp;
-      if (i == 0) return;
-      while (--i) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = this[i];
-        this[i] = this[j];
-        this[j] = temp;
+  var intersectFunc = function(array) {
+    var intersection = [];
+    array.forEach(function(item) {
+      if (this.inArray(item)) {
+        intersection.push(item);
       }
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
+    });
+    return intersection;
+  };
 
-  Object.defineProperty(proto, 'equals', {
-    value: function(array) {
-      if (this.length !== array.length) {
+  var inArrayFunc = function(item) {
+    return this.indexOf(item) !== -1;
+  };
+
+  var outerFunc = function(array) {
+    var outer = [];
+    this.forEach(function(item) {
+      if (!array.inArray(item)) {
+        outer.push(item);
+      }
+    });
+    return outer;
+  };
+
+  var shuffleFunc = function() {
+    var i = this.length,
+      j, temp;
+    if (i == 0) return;
+    while (--i) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = this[i];
+      this[i] = this[j];
+      this[j] = temp;
+    }
+  };
+
+  var equalsFunc = function(array) {
+    if (this.length !== array.length) {
+      return false;
+    }
+
+    for (var i = this.length; i--;) {
+      if (this[i] !== array[i]) {
         return false;
       }
+    }
 
-      for (var i = this.length; i--;) {
-        if (this[i] !== array[i]) {
-          return false;
-        }
-      }
+    return true;
+  };
 
-      return true;
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
+  var equivalentFunc = function(array) {
+    if (this.length != array.length) {
+      return false;
+    }
+
+    //we want to compare the string values to we will convert number 1 to string "1" since they are equivalent
+    var compareArray = this.map(function(item) {
+      return item.toString();
+    });
 
 
-  Object.defineProperty(proto, 'equivalent', {
-    value: function(array) {
-      if (this.length != array.length) {
+    for (var i = array.length; i--;) {
+      if (!compareArray.inArray(array[i].toString())) {
         return false;
       }
+    }
 
-      //we want to compare the string values to we will convert number 1 to string "1" since they are equivalent
-      var compareArray = this.map(function(item) {
-        return item.toString();
-      });
+    return true;
+  };
 
+  var cloneFunc = function() {
+    return this.map(function(item) {
+      return typeof item === "object" ? Object.create(item) : item
+    })
+  }
 
-      for (var i = array.length; i--;) {
-        if (!compareArray.inArray(array[i].toString())) {
-          return false;
-        }
-      }
+  var clearFunc = function() {
+    this.splice(0, this.length);
+  };
 
-      /*for(var i = this.length; i--;) {
-          if(this[i] != array[i]){
-              return false;
-          }
-      }*/
-
-      return true;
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
-
-
-  Object.defineProperty(proto, 'clone', {
-    value: function() {
-      return this.map(function(item) {
-        return typeof item === "object" ? Object.create(item) : item
-      })
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  });
-
-  Object.defineProperty(proto, 'clear', {
-    value: function() {
-      this.splice(0, this.length);
-    },
-    writable: false,
-    enumerable: false,
-    configurable: false
-  })
+  defineProp('first', firstFunc);
+  defineProp('remove', removeFunc);
+  defineProp('pushUnique', pushUniqueFunc);
+  defineProp('unique', uniqueFunc);
+  defineProp('intersect', intersectFunc);
+  defineProp('inArray', inArrayFunc);
+  defineProp('outer', outerFunc);
+  defineProp('shuffle', shuffleFunc);
+  defineProp('equals', equalsFunc);
+  defineProp('equivalent', equivalentFunc);
+  defineProp('clone', cloneFunc);
+  defineProp('clear', clearFunc);
 
 
 }(Array.prototype));
